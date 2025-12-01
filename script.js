05bf6dfd49ebcd5a37a07c471e1ac205150cd836
Natedogpack <njmckissick@my.milligan.edu>
2025-12-01 13:50:14 -0500

    Update alert message recipient to Nathan McKissick Designs

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.getElementById("contactForm").addEventListener("submit", function(event
   const email = event.target.email.value;
   const message = event.target.message.value;
 
-  alert(`Thanks ${name}! Your message has been prepared and will be sent to njmckissick@my.milligan.edu.\n\nMessage:\n${message}`);
+  alert(`Thanks ${name}! Your message has been prepared and will be sent to Nathan McKissick Designs.\n\nMessage:\n${message}`);
   event.target.reset();
 });
